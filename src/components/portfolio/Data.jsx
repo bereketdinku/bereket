eb38893041f14468fe6046f27004dde91f566c37
bereketdinku <bereketdinku80@gmail.com>
2024-02-12 05:00:58 -0800

    update

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -3,8 +3,9 @@ export const projectsData = [
     id: 1,
     title: "React Commerce",
     img: "/amazPro.png",
-    desc: "E-commerce website built with react and redux",
+    desc: "E-commerce website .user can navigate through different category. ",
     category: "web",
+    tech:"react and redux"
   },
   
   {
@@ -13,55 +14,56 @@ export const projectsData = [
     img: "/ethioplant.jpg",
     category: "app",
     desc: "The mobile app design to detect crop disease from their leaf and give treatment procedure.It design to help farmer and agriculture worker.",
+    tech:"AI,react native,flask and firebase"
   },
   {
     id: 3,
     title: "Booking Mobile App",
     img: "/booking.jpg",
     category: "app",
-    desc: "The mobile app design for the hotel booking.Each hotel have different kind of services.It design with react native and firebase",
+    desc: "The mobile app design for the hotel booking.Each hotel have different kind of services.The can book the hotel",
+    tech:" React native and firebase"
   },
   {
     id: 4,
     title: "Furniture E-commerce",
     img: "/furniture.PNG",
     category: "web",
-    desc: "The website is used sell different kind of furniture product.It design with react,react-redux and firebase.",
+    desc: "The website is used sell different kind of furniture product. ",
+    tech:'React,react-redux and firebase'
   },
   {
     id: 5,
     title: "Amharic Chatbot",
     img: "/AIchat.PNG",
     category: "web",
-    desc: "The chatbot design to help people with mental health problem.It design with machine learning and python UI.",
+    desc: "The chatbot design to help people with mental health problem.It has multiple question in order to help user .Based on user response question etheir have a problem or fine. ",
+    tech:'Machine Learning and Python UI'
   },
-  {
-    id: 6,
-    title: "Nike Clone",
-    img: "/nikeclone.jpg",
-    category: "app",
-    desc: "The chatbot design to help people with mental health problem.It design with machine learning and python UI.",
-  },
+ 
   {
     id: 7,
     title: "Hotel Booking Web",
     img: "/home.PNG",
     category: "web",
-    desc: "It is hotel booking website with the feature of for display list hotel,hotel detail and booking.And also it also adding new hotel and room.It design using nextjs,auth next ,clerck,tailwindcss,prisma and mongodb ",
+    desc: "It is hotel booking website with the feature of for display list hotel,hotel detail and booking.And also it also adding new hotel and room. ",
+   tecg:"Next Js,tailwindcss,Prisma,clerck,and mongodb"
   },
   {
     id: 8,
     title: "Dating Mobile App",
     img: "/dating.jpg",
     category: "app",
-    desc: "It is hotel booking website with the feature of for display list hotel,hotel detail and booking.And also it also adding new hotel and room.It design using nextjs,auth next ,clerck,tailwindcss,prisma and mongodb ",
+    desc: "It is dating app.It design to help people in the process get life partner.The user can view opposite sex profile by sliding in home page.they can also like,favorite and view detail of the user.the can also chat with other user. ",
+    tech:"Flutter ,Getx and Firebase"
   },
   {
     id: 9,
     title: "Coffee Mobile App",
     img: "/coffee.jpg",
     category: "app",
-    desc: "It is hotel booking website with the feature of for display list hotel,hotel detail and booking.And also it also adding new hotel and room.It design using nextjs,auth next ,clerck,tailwindcss,prisma and mongodb ",
+    desc: "It coffee app design sell coffee and bean .It has different category of coffee.user can order coffee ",
+    tech:"React native and redux"
   },
 ];
 
--- src/components/portfolio/WorkItems.jsx
+++ src/components/portfolio/WorkItems.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
 
 const WorkItems = ({ item }) => {
+  const [toggleState, setToggleState] = useState(0);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div className="work_card" key={WorkItems.id}>
       <img src={item.img} alt="" className="work_img" />
       <h3 className="work_title">{item.title}</h3>
-      <a href="" className="work_button">
-        Demo <i className="bx bx-right-arrow-alt work_button-icon"></i>
+      <a href="" className="work_button" onClick={() => toggleTab(1)}>
+        View more <i className="bx bx-right-arrow-alt work_button-icon"></i>
       </a>
+      <div
+            className={
+              toggleState === 1
+                ? "services_modal active-modal"
+                : "services_modal"
+            }
+          >
+            <div className="services_modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services_modal-close"
+              ></i>
+              <h3 className="services_modal-title"></h3>
+              
+              <ul className="services_modal-services grid">
+                <li className="services_modal-service">
+                  <i className="ui uil-check-circle services_modal-icon"></i>
+                  <p className="services_modal-info">
+                    {item.desc}
+                  </p>
+                </li>
+                <li className="services_modal-service">
+                  <i className="ui uil-check-circle services_modal-icon"></i>
+                  <p className="services_modal-info">{item.tech}</p>
+                </li>
+               
+              </ul>
+            </div>
+          </div>
     </div>
   );
 };
